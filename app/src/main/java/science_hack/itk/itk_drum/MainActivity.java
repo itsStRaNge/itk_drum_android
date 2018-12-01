4e25368fc3154985883839085e099db25372a3bb
Stefan_asc <stefan_aschenbrenner@gmx.de>
2018-12-01 14:26:53 +0100

    Adding button to play drum hit sound

--- app/src/main/java/science_hack/itk/itk_drum/MainActivity.java
+++ app/src/main/java/science_hack/itk/itk_drum/MainActivity.java
@@ -2,6 +2,9 @@ package science_hack.itk.itk_drum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
+    Button one = (Button) this.findViewById(R.id.button_main);
+    final MediaPlayer mp = MediaPlayer.create(this, R.raw.kick);
+
+    public void buttonClick(View view) {
+        mp.start();
+    }
 }
